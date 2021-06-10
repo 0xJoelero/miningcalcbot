113b4bdd63cb4875f397e4b24a3b721403bebcf7
Joel <joel.pereda@ripio.com>
2021-06-10 17:26:06 -0300

    alpha v0.0.2 fix action functions

--- index.js
+++ index.js
@@ -7,7 +7,10 @@ const bot = new Telegraf(TELEGRAM_TOKEN);
 let ethCurrentRate;
 let currentReward;
 let revenueResult;
-
+let gpuSelected;
+let gpuHashpower;
+let gpuMHS;
+let gpuWatts;
 // Inline buttons to select the GPUs
 let buttons = [
   [
@@ -86,7 +89,33 @@ const calculateRevenue = ({ hashpower }) => {
 bot.start((ctx) => {
   ctx.reply(
     `Hi ${ctx.chat.first_name} I'm @miningcalcbot. I'm a bot that calculate *ROI of GPU investments on Ethereum mining farms.*
-    \n\n*Commands:*
+    \n*The supported GPUS are:*
+⛏RX5700XT
+⛏RX5550XT
+⛏RX580
+⛏RTX3070
+⛏RTX3080
+⛏GTX1660S
+    \n⚠️ For now the ROI is calculated without costs of electric energy\n⚠️ Use at your own risks
+    \n*Commands:*
+    \n⛏ /help -> For consult how to use the bot.\n⛏ /calculateRoi -> Return a list of GPUs. Select one and insert the cost in USD to calculate the months for break even.
+    \n\n*Author:* @joelero \n*GitHub Repo:* https://github.com/joelperedaok/miningcalcbot`,
+    opts
+  );
+});
+
+bot.command("help", (ctx) => {
+  ctx.reply(
+    `
+    \n*The supported GPUS are:*
+⛏RX5700XT
+⛏RX5550XT
+⛏RX580
+⛏RTX3070
+⛏RTX3080
+⛏GTX1660S
+    \n⚠️ For now the ROI is calculated without costs of electric energy\n⚠️ Use at your own risks
+    \n*Commands:*
     \n⛏ /help -> For consult how to use the bot.\n⛏ /calculateRoi -> Return a list of GPUs. Select one and insert the cost in USD to calculate the months for break even.
     \n\n*Author:* @joelero \n*GitHub Repo:* https://github.com/joelperedaok/miningcalcbot`,
     opts
@@ -104,200 +133,98 @@ bot.command("calculateRoi", (ctx) => {
   });
 });
 
-// If user select 5700
 bot.action("5700", (ctx1) => {
+  gpuSelected = ctx1.callbackQuery.data;
   ethRate();
   rewardResult();
-  ctx1.reply("*GPU RX5700XT* \nNow please send the cost in USD ⤵", opts);
-  // When the user insert and send the cost
-  bot.on("message", (ctx) => {
-    let userResponse = ctx.message.text;
-    if (!isNaN(userResponse)) {
-      // if is numeric
-      calculateRevenue({ hashpower: 55000000 });
-      let monthlyRevenueInUsd =
-        parseFloat(revenueResult) * parseFloat(ethCurrentRate);
-      let monthlyRevenueInUsdConverted =
-        convertExponentialToDecimal(monthlyRevenueInUsd) * 30;
-      let gpuRoi =
-        Number(userResponse) / parseFloat(monthlyRevenueInUsdConverted);
-      let fixedRoi = gpuRoi.toFixed(2);
-
-      ctx.reply(
-        `*1 RX5700XT:*
-      \n\nMHS: 55
-      \nWatts: 115w
-      \nCost: $${ctx.message.text}
-      \n*Months for breakeven: ${fixedRoi}*`,
-        opts
-      );
-    } else {
-      ctx.reply(
-        `⚠️ The response must be a number without symbols and letters. Only a number, i.e.: *2000*`,
-        opts
-      );
-    }
-  });
-});
-bot.action("580", (ctx1) => {
-  ethRate();
-  rewardResult();
-  ctx1.reply("*GPU RX580* \nNow please send the cost in USD ⤵", opts);
-  bot.on("message", (ctx) => {
-    let userResponse = ctx.message.text;
-    if (!isNaN(userResponse)) {
-      calculateRevenue({ hashpower: 34000000 });
-      let monthlyRevenueInUsd =
-        parseFloat(revenueResult) * parseFloat(ethCurrentRate);
-      let monthlyRevenueInUsdConverted =
-        convertExponentialToDecimal(monthlyRevenueInUsd) * 30;
-      let gpuRoi =
-        Number(ctx.message.text) / parseFloat(monthlyRevenueInUsdConverted);
-      let fixedRoi = gpuRoi.toFixed(2);
-
-      ctx.reply(
-        `*1 RX580:*
-      \n\nMHS: 34
-      \nWatts: 130w
-      \nCost: $${ctx.message.text}
-      \n*Months for breakeven: ${fixedRoi}*`,
-        opts
-      );
-    } else {
-      ctx.reply(
-        `⚠️ The response must be a number without symbols and letters. Only a number, i.e.: *2000*`,
-        opts
-      );
-    }
-  });
-});
-bot.action("3070", (ctx1) => {
-  ethRate();
-  rewardResult();
-  ctx1.reply("*GPU RTX3070* \nNow please send the cost in USD ⤵", opts);
-  bot.on("message", (ctx) => {
-    let userResponse = ctx.message.text;
-    if (!isNaN(userResponse)) {
-      calculateRevenue({ hashpower: 59000000 });
-      let monthlyRevenueInUsd =
-        parseFloat(revenueResult) * parseFloat(ethCurrentRate);
-      let monthlyRevenueInUsdConverted =
-        convertExponentialToDecimal(monthlyRevenueInUsd) * 30;
-      let gpuRoi =
-        Number(ctx.message.text) / parseFloat(monthlyRevenueInUsdConverted);
-      let fixedRoi = gpuRoi.toFixed(2);
-
-      ctx.reply(
-        `*1 RTX3070:*
-      \n\nMHS: 59
-      \nWatts: 125w
-      \nCost: $${ctx.message.text}
-      \n*Months for breakeven: ${fixedRoi}*`,
-        opts
-      );
-    } else {
-      ctx.reply(
-        `⚠️ The response must be a number without symbols and letters. Only a number, i.e.: *2000*`,
-        opts
-      );
-    }
-  });
-});
-bot.action("3080", (ctx1) => {
-  ethRate();
-  rewardResult();
-  ctx1.reply("*GPU RTX3080* \nNow please send the cost in USD ⤵", opts);
-  bot.on("message", (ctx) => {
-    let userResponse = ctx.message.text;
-    if (!isNaN(userResponse)) {
-      calculateRevenue({ hashpower: 95000000 });
-      let monthlyRevenueInUsd =
-        parseFloat(revenueResult) * parseFloat(ethCurrentRate);
-      let monthlyRevenueInUsdConverted =
-        convertExponentialToDecimal(monthlyRevenueInUsd) * 30;
-      let gpuRoi =
-        Number(ctx.message.text) / parseFloat(monthlyRevenueInUsdConverted);
-      let fixedRoi = gpuRoi.toFixed(2);
-
-      ctx.reply(
-        `*1 RTX3080:*
-      \n\nMHS: 95
-      \nWatts: 135w
-      \nCost: $${ctx.message.text}
-      \n*Months for breakeven: ${fixedRoi}*`,
-        opts
-      );
-    } else {
-      ctx.reply(
-        `⚠️ The response must be a number without symbols and letters. Only a number, i.e.: *2000*`,
-        opts
-      );
-    }
-  });
-});
-bot.action("1660", (ctx1) => {
-  ethRate();
-  rewardResult();
-  ctx1.reply("*GPU GTX1660* \nNow please send the cost in USD ⤵", opts);
-  bot.on("message", (ctx) => {
-    let userResponse = ctx.message.text;
-    if (!isNaN(userResponse)) {
-      calculateRevenue({ hashpower: 30000000 });
-      let monthlyRevenueInUsd =
-        parseFloat(revenueResult) * parseFloat(ethCurrentRate);
-      let monthlyRevenueInUsdConverted =
-        convertExponentialToDecimal(monthlyRevenueInUsd) * 30;
-      let gpuRoi =
-        Number(ctx.message.text) / parseFloat(monthlyRevenueInUsdConverted);
-      let fixedRoi = gpuRoi.toFixed(2);
-
-      ctx.reply(
-        `*1 GTX1660:*
-      \n\nMHS: 30
-      \nWatts: 80w
-      \nCost: $${ctx.message.text}
-      \n*Months for breakeven: ${fixedRoi}*`,
-        opts
-      );
-    } else {
-      ctx.reply(
-        `⚠️ The response must be a number without symbols and letters. Only a number, i.e.: *2000*`,
-        opts
-      );
-    }
-  });
+  ctx1.reply("*GPU RX5700XT* \nNow please send the cost in USD of  ⤵", opts);
 });
 bot.action("5500", (ctx1) => {
+  gpuSelected = ctx1.callbackQuery.data;
   ethRate();
   rewardResult();
   ctx1.reply("*GPU RX5500XT* \nNow please send the cost in USD of  ⤵", opts);
-  bot.on("message", (ctx) => {
-    let userResponse = ctx.message.text;
-    if (!isNaN(userResponse)) {
-      calculateRevenue({ hashpower: 26000000 });
-      let monthlyRevenueInUsd =
-        parseFloat(revenueResult) * parseFloat(ethCurrentRate);
-      let monthlyRevenueInUsdConverted =
-        convertExponentialToDecimal(monthlyRevenueInUsd) * 30;
-      let gpuRoi =
-        Number(ctx.message.text) / parseFloat(monthlyRevenueInUsdConverted);
-      let fixedRoi = gpuRoi.toFixed(2);
+});
+bot.action("580", (ctx1) => {
+  gpuSelected = ctx1.callbackQuery.data;
+  ethRate();
+  rewardResult();
+  ctx1.reply("*GPU RX580* \nNow please send the cost in USD of  ⤵", opts);
+});
+bot.action("3070", (ctx1) => {
+  gpuSelected = ctx1.callbackQuery.data;
+  console.log(`gpuSelected`, gpuSelected);
+  ethRate();
+  rewardResult();
+  ctx1.reply("*GPU RTX3070* \nNow please send the cost in USD of  ⤵", opts);
+});
+bot.action("3080", (ctx1) => {
+  gpuSelected = ctx1.callbackQuery.data;
+  ethRate();
+  rewardResult();
+  ctx1.reply("*GPU RTX3080* \nNow please send the cost in USD of  ⤵", opts);
+});
+bot.action("1660", (ctx1) => {
+  gpuSelected = ctx1.callbackQuery.data;
+  console.log(`gpuSelected`, gpuSelected);
+  ethRate();
+  rewardResult();
+  ctx1.reply("*GPU GTX1660S* \nNow please send the cost in USD of  ⤵", opts);
+});
 
-      ctx.reply(
-        `*1 RX5500XT:*
-      \n\nMHS: 26
-      \nWatts: 85w
-      \nCost: $${ctx.message.text}
-      \n*Months for breakeven: ${fixedRoi}*`,
-        opts
-      );
-    } else {
-      ctx.reply(
-        `⚠️ The response must be a number without symbols and letters. Only a number, i.e.: *2000*`,
-        opts
-      );
-    }
-  });
+bot.on("message", (ctx) => {
+  let userResponse = ctx.message.text;
+
+  if (gpuSelected === "5500") {
+    gpuHashpower = 26000000;
+    gpuMHS = "26mhs";
+    gpuWatts = "80w";
+  } else if (gpuSelected === "5700") {
+    gpuHashpower = 55000000;
+    gpuMHS = "55mhs";
+    gpuWatts = "120w";
+  } else if (gpuSelected === "3070") {
+    gpuHashpower = 59000000;
+    gpuMHS = "59mhs";
+    gpuWatts = "120w";
+  } else if (gpuSelected === "580") {
+    gpuHashpower = 34000000;
+    gpuMHS = "34mhs";
+    gpuWatts = "135w";
+  } else if (gpuSelected === "3080") {
+    gpuHashpower = 96000000;
+    gpuMHS = "96mhs";
+    gpuWatts = "145w";
+  } else if (gpuSelected === "1660") {
+    gpuHashpower = 31000000;
+    gpuMHS = "31mhs";
+    gpuWatts = "75w";
+  }
+
+  if (!isNaN(userResponse)) {
+    calculateRevenue({ hashpower: gpuHashpower });
+    let monthlyRevenueInUsd =
+      parseFloat(revenueResult) * parseFloat(ethCurrentRate);
+    let monthlyRevenueInUsdConverted =
+      convertExponentialToDecimal(monthlyRevenueInUsd) * 30;
+    let gpuRoi =
+      Number(ctx.message.text) / parseFloat(monthlyRevenueInUsdConverted);
+    let fixedRoi = gpuRoi.toFixed(2);
+
+    ctx.reply(
+      `*Result for 1 ${gpuSelected}:*
+  \n\nMHS: ${gpuMHS}
+  \nWatts: ${gpuWatts}
+  \nCost: $${ctx.message.text}
+  \n*Months for breakeven: ${fixedRoi}*`,
+      opts
+    );
+  } else {
+    ctx.reply(
+      `⚠️ The response must be a number without symbols and letters. Only a number, i.e.: *2000*`,
+      opts
+    );
+  }
 });
 
 bot.launch();
