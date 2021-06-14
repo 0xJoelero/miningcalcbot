451309963bc0ec6ae51ff20f9a13a6539364827b
Joel <joel.pereda@ripio.com>
2021-06-14 10:54:31 -0300

    alpha v0.0.4 add more GPUs

---
+++ buttons.js
@@ -0,0 +1,86 @@
+export const selectGpuButtons = [
+  [
+    {
+      text: "570 8GB",
+      callback_data: "570",
+    },
+    {
+      text: "580 8GB",
+      callback_data: "580",
+    },
+    {
+      text: "5500 XT",
+      callback_data: "5500",
+    },
+  ],
+  [
+    {
+      text: "5600 XT",
+      callback_data: "5600",
+    },
+    {
+      text: "6700 XT",
+      callback_data: "6700XT",
+    },
+    {
+      text: "6800 XT",
+      callback_data: "6700XT",
+    },
+  ],
+  [
+    {
+      text: "1080",
+      callback_data: "1080",
+    },
+    {
+      text: "1080Ti",
+      callback_data: "1080Ti",
+    },
+    {
+      text: "1660S",
+      callback_data: "1660s",
+    },
+  ],
+  [
+    {
+      text: "2060",
+      callback_data: "2060",
+    },
+    {
+      text: "2070",
+      callback_data: "2070",
+    },
+    {
+      text: "2080",
+      callback_data: "2080",
+    },
+  ],
+  [
+    {
+      text: "2080Ti",
+      callback_data: "2080Ti",
+    },
+    {
+      text: "3060",
+      callback_data: "3060",
+    },
+    {
+      text: "3060Ti",
+      callback_data: "3060Ti",
+    },
+  ],
+  [
+    {
+      text: "3070",
+      callback_data: "3070",
+    },
+    {
+      text: "3080",
+      callback_data: "3080",
+    },
+    {
+      text: "3090",
+      callback_data: "3090",
+    },
+  ],
+];
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { Telegraf } = require("telegraf");
 const { getReward, getEthPrice } = require("./apis");
+const { selectGpuButtons } = require("./buttons");
 const { TELEGRAM_TOKEN } = require("./environment");
 
 const bot = new Telegraf(TELEGRAM_TOKEN);
@@ -12,44 +13,6 @@ let gpuHashpower;
 let gpuMHS;
 let gpuWatts;
 // Inline buttons to select the GPUs
-let buttons = [
-  [
-    {
-      text: "RX 580 8GB",
-      callback_data: "580",
-    },
-    {
-      text: "RX 570 8GB",
-      callback_data: "570",
-    },
-  ],
-  [
-    {
-      text: "RTX 3070",
-      callback_data: "3070",
-    },
-    {
-      text: "RTX 3080",
-      callback_data: "3080",
-    },
-  ],
-  [
-    {
-      text: "GTX 1660 Super",
-      callback_data: "1660",
-    },
-    {
-      text: "RX 5500",
-      callback_data: "5500",
-    },
-  ],
-  [
-    {
-      text: "RX 5700XT",
-      callback_data: "5700",
-    },
-  ],
-];
 
 // Options to set markdown in messasges
 const opts = {
@@ -135,17 +98,23 @@ bot.command("calculateRoi", (ctx) => {
   let roiWelcome = `*Select a GPU to calculate the break even ⤵*`;
   bot.telegram.sendMessage(ctx.chat.id, roiWelcome, {
     reply_markup: {
-      inline_keyboard: buttons,
+      inline_keyboard: selectGpuButtons,
     },
     parse_mode: "Markdown",
   });
 });
 
-bot.action("5700", (ctx1) => {
+bot.action("570", (ctx1) => {
   gpuSelected = ctx1.callbackQuery.data;
   ethRate();
   rewardResult();
-  ctx1.reply("*GPU RX5700XT* \nNow please send the cost in USD of  ⤵", opts);
+  ctx1.reply("*GPU RX570 8GB* \nNow please send the cost in USD of  ⤵", opts);
+});
+bot.action("580", (ctx1) => {
+  gpuSelected = ctx1.callbackQuery.data;
+  ethRate();
+  rewardResult();
+  ctx1.reply("*GPU RX580 8GB* \nNow please send the cost in USD of  ⤵", opts);
 });
 bot.action("5500", (ctx1) => {
   gpuSelected = ctx1.callbackQuery.data;
@@ -156,11 +125,96 @@ bot.action("5500", (ctx1) => {
     opts
   );
 });
-bot.action("580", (ctx1) => {
+
+bot.action("5600", (ctx1) => {
+  gpuSelected = ctx1.callbackQuery.data;
+  console.log(`gpuSelected`, gpuSelected);
+  ethRate();
+  rewardResult();
+  ctx1.reply("*GPU RX5600* \nNow please send the cost in USD of  ⤵", opts);
+});
+bot.action("5700", (ctx1) => {
   gpuSelected = ctx1.callbackQuery.data;
   ethRate();
   rewardResult();
-  ctx1.reply("*GPU RX580 8GB* \nNow please send the cost in USD of  ⤵", opts);
+  ctx1.reply("*GPU RX5700XT* \nNow please send the cost in USD of  ⤵", opts);
+});
+bot.action("6700XT", (ctx1) => {
+  gpuSelected = ctx1.callbackQuery.data;
+  console.log(`gpuSelected`, gpuSelected);
+  ethRate();
+  rewardResult();
+  ctx1.reply("*GPU RX6700XT* \nNow please send the cost in USD of  ⤵", opts);
+});
+bot.action("6800XT", (ctx1) => {
+  gpuSelected = ctx1.callbackQuery.data;
+  console.log(`gpuSelected`, gpuSelected);
+  ethRate();
+  rewardResult();
+  ctx1.reply("*GPU RX6800XT* \nNow please send the cost in USD of  ⤵", opts);
+});
+bot.action("1080", (ctx1) => {
+  gpuSelected = ctx1.callbackQuery.data;
+  console.log(`gpuSelected`, gpuSelected);
+  ethRate();
+  rewardResult();
+  ctx1.reply("*GPU 1080* \nNow please send the cost in USD of  ⤵", opts);
+});
+bot.action("1080Ti", (ctx1) => {
+  gpuSelected = ctx1.callbackQuery.data;
+  console.log(`gpuSelected`, gpuSelected);
+  ethRate();
+  rewardResult();
+  ctx1.reply("*GPU 1080Ti* \nNow please send the cost in USD of  ⤵", opts);
+});
+bot.action("1660s", (ctx1) => {
+  gpuSelected = ctx1.callbackQuery.data;
+  console.log(`gpuSelected`, gpuSelected);
+  ethRate();
+  rewardResult();
+  ctx1.reply("*GPU GTX1660S* \nNow please send the cost in USD of  ⤵", opts);
+});
+bot.action("2060", (ctx1) => {
+  gpuSelected = ctx1.callbackQuery.data;
+  console.log(`gpuSelected`, gpuSelected);
+  ethRate();
+  rewardResult();
+  ctx1.reply("*GPU 2060* \nNow please send the cost in USD of  ⤵", opts);
+});
+bot.action("2070", (ctx1) => {
+  gpuSelected = ctx1.callbackQuery.data;
+  console.log(`gpuSelected`, gpuSelected);
+  ethRate();
+  rewardResult();
+  ctx1.reply("*GPU 2070* \nNow please send the cost in USD of  ⤵", opts);
+});
+bot.action("2080", (ctx1) => {
+  gpuSelected = ctx1.callbackQuery.data;
+  console.log(`gpuSelected`, gpuSelected);
+  ethRate();
+  rewardResult();
+  ctx1.reply("*GPU 2080* \nNow please send the cost in USD of  ⤵", opts);
+});
+bot.action("2080Ti", (ctx1) => {
+  gpuSelected = ctx1.callbackQuery.data;
+  console.log(`gpuSelected`, gpuSelected);
+  ethRate();
+  rewardResult();
+  ctx1.reply("*GPU 2080Ti* \nNow please send the cost in USD of  ⤵", opts);
+});
+bot.action("3060", (ctx1) => {
+  gpuSelected = ctx1.callbackQuery.data;
+  console.log(`gpuSelected`, gpuSelected);
+  ethRate();
+  rewardResult();
+  ctx1.reply("*GPU 3060* \nNow please send the cost in USD of  ⤵", opts);
+});
+bot.action("3060Ti", (ctx1) => {
+  gpuSelected = ctx1.callbackQuery.data;
+  console.log(`gpuSelected`, gpuSelected);
+  ethRate();
+  rewardResult();
+  ctx1.reply("*GPU 3060Ti* \nNow please send the cost in USD of  ⤵", opts);
 });
 bot.action("3070", (ctx1) => {
   gpuSelected = ctx1.callbackQuery.data;
@@ -175,52 +229,92 @@ bot.action("3080", (ctx1) => {
   rewardResult();
   ctx1.reply("*GPU RTX3080* \nNow please send the cost in USD of  ⤵", opts);
 });
-bot.action("1660", (ctx1) => {
+bot.action("3090", (ctx1) => {
   gpuSelected = ctx1.callbackQuery.data;
-  console.log(`gpuSelected`, gpuSelected);
   ethRate();
   rewardResult();
-  ctx1.reply("*GPU GTX1660S* \nNow please send the cost in USD of  ⤵", opts);
-});
-bot.action("570", (ctx1) => {
-  gpuSelected = ctx1.callbackQuery.data;
-  console.log(`gpuSelected`, gpuSelected);
-  ethRate();
-  rewardResult();
-  ctx1.reply("*GPU RX570 8GB* \nNow please send the cost in USD of  ⤵", opts);
+  ctx1.reply("*GPU RTX3090* \nNow please send the cost in USD of  ⤵", opts);
 });
 
 bot.on("message", (ctx) => {
   let userResponse = ctx.message.text;
 
-  if (gpuSelected === "5500") {
-    gpuHashpower = 26000000;
-    gpuMHS = "26mhs";
-    gpuWatts = "80w";
-  } else if (gpuSelected === "5700") {
-    gpuHashpower = 55000000;
-    gpuMHS = "55mhs";
-    gpuWatts = "120w";
-  } else if (gpuSelected === "3070") {
-    gpuHashpower = 59000000;
-    gpuMHS = "59mhs";
-    gpuWatts = "120w";
+  if (gpuSelected === "570") {
+    gpuHashpower = 31000000;
+    gpuMHS = "31mhs";
+    gpuWatts = "85w";
   } else if (gpuSelected === "580") {
     gpuHashpower = 34000000;
     gpuMHS = "34mhs";
     gpuWatts = "135w";
+  } else if (gpuSelected === "5500") {
+    gpuHashpower = 26000000;
+    gpuMHS = "26mhs";
+    gpuWatts = "80w";
+  } else if (gpuSelected === "5600") {
+    gpuHashpower = 40000000;
+    gpuMHS = "40mhs";
+    gpuWatts = "110w";
+  } else if (gpuSelected === "5700") {
+    gpuHashpower = 55000000;
+    gpuMHS = "55mhs";
+    gpuWatts = "120w";
+  } else if (gpuSelected === "6700XT") {
+    gpuHashpower = 48000000;
+    gpuMHS = "48mhs";
+    gpuWatts = "140w";
+  } else if (gpuSelected === "6800XT") {
+    gpuHashpower = 54000000;
+    gpuMHS = "54mhs";
+    gpuWatts = "150w";
+  } else if (gpuSelected === "1080") {
+    gpuHashpower = 34000000;
+    gpuMHS = "34mhs";
+    gpuWatts = "170w";
+  } else if (gpuSelected === "1080Ti") {
+    gpuHashpower = 39000000;
+    gpuMHS = "39mhs";
+    gpuWatts = "180w";
+  } else if (gpuSelected === "1660s") {
+    gpuHashpower = 31000000;
+    gpuMHS = "31mhs";
+    gpuWatts = "75w";
+  } else if (gpuSelected === "2060") {
+    gpuHashpower = 30000000;
+    gpuMHS = "30mhs";
+    gpuWatts = "120w";
+  } else if (gpuSelected === "2070") {
+    gpuHashpower = 40000000;
+    gpuMHS = "40mhs";
+    gpuWatts = "140w";
+  } else if (gpuSelected === "2080") {
+    gpuHashpower = 41000000;
+    gpuMHS = "41mhs";
+    gpuWatts = "160w";
+  } else if (gpuSelected === "2080Ti") {
+    gpuHashpower = 55000000;
+    gpuMHS = "55mhs";
+    gpuWatts = "180w";
+  } else if (gpuSelected === "3060") {
+    gpuHashpower = 37000000;
+    gpuMHS = "37mhs";
+    gpuWatts = "110w";
+  } else if (gpuSelected === "3060Ti") {
+    gpuHashpower = 58000000;
+    gpuMHS = "58mhs";
+    gpuWatts = "130w";
+  } else if (gpuSelected === "3070") {
+    gpuHashpower = 59000000;
+    gpuMHS = "59mhs";
+    gpuWatts = "120w";
   } else if (gpuSelected === "3080") {
     gpuHashpower = 96000000;
     gpuMHS = "96mhs";
     gpuWatts = "145w";
-  } else if (gpuSelected === "1660") {
-    gpuHashpower = 31000000;
-    gpuMHS = "31mhs";
-    gpuWatts = "75w";
-  } else if (gpuSelected === "570") {
-    gpuHashpower = 31000000;
-    gpuMHS = "31mhs";
-    gpuWatts = "85w";
+  } else if (gpuSelected === "3090") {
+    gpuHashpower = 114000000;
+    gpuMHS = "114mhs";
+    gpuWatts = "320w";
   }
 
   if (!isNaN(userResponse)) {
