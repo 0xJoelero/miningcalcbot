683bca2cc6df739cd8ded7af3bf5214b65367729
Ripio <ripio@Tos-MacBook-Pro.local>
2021-06-27 21:03:56 -0300

    add spanish translations

--- translations.js
+++ translations.js
@@ -24,6 +24,28 @@ i18n.init({
         "calculateRoi.error": `⚠️ The response must be a valid supported GPU, please select it from te buttons above. ⚠️`,
       },
     },
+    es: {
+      translations: {
+        "reply.welcome":
+          `Hola {{user}}! Soy @miningcalcbot.\nSoy un bot que calcula el *ROI de inversiones en GPUs para mineria de Ethereum.*"` +
+          `\n\n⚠️ Este bot esta en ALPHA, uselo bajo su propio riesgo ⚠️ ` +
+          `\n\n*Commandos:* \n/help -> Obtiene informacion sobre el bot y los comandos,\n/calculateRoi -> Muestra una lista de GPUs con la que puedes calcular el ROI.` +
+          `\n\n\n*Autor*: @Joelero \n*Github*: https://github.com/joelperedaok/miningcalcbot `,
+        "reply.help":
+          `/help -> Lista los comandos` +
+          `\n/calculateRoi -> Calcula el ROI para las GPUs que el bot soporta`,
+        "calculateRoi.select_gpu": `*Selecciona una GPU para calcular el punto de equilibrio:*`,
+        "calculateRoi.selected_gpu": `*{{gpu_model}}* \nPor favor dime el costo en USD:`,
+        "calculateRoi.result":
+          `*Resultado para 1 {{gpu_selected}}:*` +
+          `\n\nMH/s: {{gpu_hashrate}}` +
+          `\nWatts: {{gpu_watts}}` +
+          `\nCosto: {{gpu_cost}}` +
+          `\nGanancia mensual: {{gpu_monthly_revenue}}` +
+          `\nROI: {{gpu_roi}}`,
+        "calculateRoi.error": `⚠️ La respuesta debe ser una GPU válida, selecciónela de los botones de arriba. ⚠️`,
+      },
+    },
   },
   fallbackLng: "en",
   debug: true,
